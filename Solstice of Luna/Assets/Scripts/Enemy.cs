81cea697fb6c239bffdd784cfe9bf5c4ae548c4c
lauanS <lauan.s.souza@gmail.com>
2021-10-24 05:39:28 -0300

    Barra de vida fixa em cima do player

--- Solstice of Luna/Assets/Scripts/Enemy.cs
+++ Solstice of Luna/Assets/Scripts/Enemy.cs
@@ -6,8 +6,17 @@ public class Enemy : MonoBehaviour
 {
     Animator anim;
 
+    public HealthSystem healthSystem;
+    public Transform pfHealthBar;
     void Start() {
         anim = GetComponent<Animator>();
+
+        healthSystem = new HealthSystem(100);
+
+        Transform healthBarTransform = Instantiate(pfHealthBar, new Vector3(-0.25f, -0.3f), Quaternion.identity);
+        HealthBar healthBar = healthBarTransform.GetComponent<HealthBar>();
+
+        healthBar.Setup(healthSystem);
     }
 
     void Update() {
@@ -17,7 +26,7 @@ public class Enemy : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "Player") {
             anim.SetTrigger("Attack");
-            Destroy(collision.gameObject);
+            // Destroy(collision.gameObject);
         }
     }
 }
