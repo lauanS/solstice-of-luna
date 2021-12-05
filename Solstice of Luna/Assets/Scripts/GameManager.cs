97be837990d91ccd0aa681cb61071954e2df14e9
lauanS <lauan.s.souza@gmail.com>
2021-12-05 17:12:54 -0300

    Ajustando tamanho do mapa & posição dos inimigos

--- Solstice of Luna/Assets/Scripts/GameManager.cs
+++ Solstice of Luna/Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour {
     public Transform pfSlime;
@@ -28,15 +29,17 @@ public class GameManager : MonoBehaviour {
         // Setup dos inimigos
         Enemy.Setup();
 
-        Enemy slime = Enemy.Create(new Vector3(-19, -25, 0), pfSlime);
-        Enemy slime2 = Enemy.Create(new Vector3(-15.5f, -25.9f, 0), pfSlime);
-        Enemy slime3 = Enemy.Create(new Vector3(-11.5f, -25.9f, 0), pfSlime);
-        Enemy slime4 = Enemy.Create(new Vector3(-9.5f, -25.9f, 0), pfSlime);
-        Enemy slime5 = Enemy.Create(new Vector3(-7.6f, -25.9f, 0), pfSlime);
-        Enemy slime6 = Enemy.Create(new Vector3(-2.5f, -25.9f, 0), pfSlime);
-        Enemy slime7 = Enemy.Create(new Vector3(5f, -25.9f, 0), pfSlime);
-        Enemy slime8 = Enemy.Create(new Vector3(9f, -25.9f, 0), pfSlime);
-        Enemy slime9 = Enemy.Create(new Vector3(14f, -25.9f, 0), pfSlime);
+        int enemyAmount = 20;
+        int defaultEnemyPosX = 0;
+        int enemyPosY = -27, enemyPosX = 0;
+
+        // Instanciando inimigos aleatóriamente dentro de um intervalo
+        for (int i = 0; i < enemyAmount; i++) {
+            enemyPosX = Random.Range(defaultEnemyPosX, defaultEnemyPosX + Random.Range(-12, 12));
+            enemyPosY = Random.Range(enemyPosY, enemyPosY - 5);
+
+            Enemy.Create(new Vector3(enemyPosX, enemyPosY, 0), pfSlime);
+        }
 
         // Setup do player
         player = GameObject.FindGameObjectWithTag("Player");
